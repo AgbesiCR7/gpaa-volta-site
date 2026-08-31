2adc7c4ab60ee1168a3c0a8795bf9763798ca6a8
AgbesiCR7 <samlafoaa@gmail.com>
2026-08-31 16:28:35 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.GPAA_CONFIG = {
+  SUPABASE_URL: 'https://xceyjczfwbtyuwjadyry.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: sb_publishable__kzOC8f7Hrcr3MGW7JTflA_fVFRbKLH,
+  GOOGLE_MAPS_API_KEY: ''
+};
